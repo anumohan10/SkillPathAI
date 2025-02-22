ffce4a7b053f6236cd78022fe10ee588bdab8f6b
anumohan10 <anusreemohanan22@gmail.com>
2025-02-21 23:31:49 -0500

    "Changed Fetch kaggle code"

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,17 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+print(sys.path)
 from backend.etl.extract.fetch_kaggle_dataset import fetch_kaggle_dataset
 from backend.etl.load.load_to_snowflake import upload_to_snowflake
 
 # Define datasets and their corresponding stages
 datasets = {
-    "udemy": {"dataset_id": "mortezasalarkia/udemy-courses-meta-data-110k-courses", "stage": "UDEMY_STAGE"},
-    "coursera": {"dataset_id": "anusreemohanan/coursera-course-details", "stage": "COURSERA_STAGE"},
-    "edx": {"dataset_id": "santoshapatil31/edx-all-courses-3082-courses", "stage": "EDX_STAGE"},
-    "udacity": {"dataset_id": "anusreemohanan/udacity-courses", "stage": "UDACITY_STAGE"},
-    "pluralsight": {"dataset_id": "anusreemohanan/pluralsight-dataset", "stage": "PLURALSIGHT_STAGE"}
+    "udemy": {"dataset_id": "mortezasalarkia/udemy-courses-meta-data-110k-courses", "filename": "udemy.csv", "stage": "UDEMY_STAGE"},
+    "coursera": {"dataset_id": "anusreemohanan/coursera-course-details", "filename": "coursera.csv","stage": "COURSERA_STAGE"},
+    "edx": {"dataset_id": "santoshapatil31/edx-all-courses-3082-courses", "filename": "edx.csv","stage": "EDX_STAGE"},
+    "udacity": {"dataset_id": "anusreemohanan/udacity-courses", "filename": "udacity.csv", "stage": "UDACITY_STAGE"},
+    "pluralsight": {"dataset_id": "anusreemohanan/pluralsight-dataset", "filename": "pluralsight.csv", "stage": "PLURALSIGHT_STAGE"}
 }
 
 # Process each dataset
@@ -20,7 +20,7 @@ for name, details in datasets.items():
     print(f"Processing {name} dataset...")
 
     # Fetch and compress dataset
-    output_path = fetch_kaggle_dataset(details["dataset_id"])
+    output_path = fetch_kaggle_dataset(details["dataset_id"], details["filename"])
     file_name_gz = output_path.split("/")[-1]  # Get the name of the gzipped file
 
     # Upload compressed file to the designated stage
